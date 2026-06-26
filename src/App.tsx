1b1e78a9c49c6508bce0ce19cdb524156bb0c278
Javohir-Senior <javohiribrohimov644@gmail.com>
2026-06-26 14:26:06 +0500

    yangilanish

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import Admin from "./Admin"
 import Dashboard from "./dashboard"
 import { Route, Routes } from "react-router-dom"
-import Stollar from "./Stollar"
-import Statistika from "./Statistika"
 
 const App = () => {
   return (
@@ -10,9 +8,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Dashboard/>} />
         <Route path="/admin" element={<Admin/>} />
-        <Route path="/stollar" element={<Stollar/>} />
-        <Route path="/statistika" element={<Statistika/>} />
-        <Route path="/hisobot" element={<Statistika/>} />
       </Routes>
     </div>
   )
